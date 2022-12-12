4efd65ebe914e3d7a0602c6a523a6b512814a294
pickledim <dimitrisglenis@gmail.com>
2022-12-12 10:15:21 +0100

    Update of climb equations

--- equations_of_motion.py
+++ equations_of_motion.py
@@ -23,37 +23,6 @@ def get_thrust(speed, coefs):
     return _thrust
 
 
-def find_alpha(_thrust, _alpha_vect, _zp, _v, _gamma):
-    """
-
-    :param _thrust:
-    :param _alpha_vect:
-    :param _zp:
-    :param _v:
-    :param _gamma:
-    :return:
-    """
-    T_aero = _thrust * np.cos(np.radians(_alpha_vect))
-    drag = 0.5 * rho * S * f_cd(_alpha_vect) * v ** 2
-    SFx = T_aero - drag - weight * np.sin(np.radians(gamma))
-
-    _accel = SFx / mass
-
-    df = pd.DataFrame([_alpha_vect, _accel, drag])
-    df = df.T
-    df.columns = ['alpha', 'accel', 'drag']
-    df = df[df.accel > 0]
-    ind_min = df.accel.idxmin()
-
-    _min_accel = df.loc[ind_min, 'accel']
-
-    _aoa = df.loc[ind_min, 'alpha']
-
-    _drag = df.loc[ind_min, 'drag']
-
-    return _min_accel, _aoa, _drag
-
-
 # get config data
 with open('config.json', 'r') as f:
     config_data = json.load(f)
@@ -80,7 +49,10 @@ v_stall = config_data['v_stall']
 
 # v2min definition
 vmuv2_lim = 1.17*v_stall
-v2min = max(1.13*v_stall, 1.1*vmca, vmuv2_lim)
+# v_grad = uc.ms2kt(78.47452364358746)
+
+v2min = max(1.13*v_stall, 1.1*vmca, vmuv2_lim)#, v_grad)
+
 
 # get vR min
 vr = max(1.05*v_stall, 1.05*vmca)
@@ -107,24 +79,28 @@ f_cl = interp1d(np.array([aoa0, aoa_max]),
 f_cd = interp1d(np.array([aoa0, aoa_max]),
                 np.array([cd_max, cd_max]))
 
-vr += 0.075*v_stall
+vr += .12*v_stall
 # define vef
 vef = vr - 2
 
 
 v = 0.
-v_kt = uc.ms2kt(v)
 t = 0.
 x = 0.
+gamma = 0
+gamma_rad = 0
+vz = 0
+height = 0
 
 dt = 0.125
+v_kt = uc.ms2kt(v)
 vef_inst = True
 print('\n====== Ground Phase ======\n')
 while v_kt < vr:
 
     if vef_inst:
         if v_kt >= vef:
-            thrust = thrust / 2  # + 1e4 thrust_oei#
+            thrust = 100123.43#thrust / 2  # + 1e4 thrust_oei#
             vef_inst = False
         else:
             thrust = get_thrust(v, engine_coefs)  # trhust_aeo + 1.8e4
@@ -156,11 +132,9 @@ while round(lift, 1) < round(weight, 1):
 
     aoa = q * dt + aoa0
     if aoa > aoa_max:
-
         print('Increase VR the a/c cannot Lift Off :(')
         increase = True
         break
-
     else:
         drag = 0.5 * rho * S * f_cd(aoa) * v ** 2
         lift = 0.5 * rho * S * f_cl(aoa) * v ** 2
@@ -181,50 +155,46 @@ while round(lift, 1) < round(weight, 1):
     aoa0 = aoa
 print(f'Velocity: {round(v_kt, 2)} kt')
 print(f'Distance: {round(x, 2)} m')
+print(f'Aoa: {round(aoa, 2)} deg')
 
 vlof = v_kt
-gamma = 0
-vz = 0
-
 teta0 = aoa
 aoa0 = aoa
-height = 0
+
 print('\n====== Airborne Phase ======\n')
 while height < 35.:
 
-    _alpha_vect = np.linspace(config_data['alpha_min'], aoa0, 100)
+    T_aero = thrust * np.cos(np.radians(aoa_max))
 
-    min_accel, aoa, _drag = find_alpha(thrust, _alpha_vect, zp, v, gamma)
+    drag = 0.5 * rho * S * f_cd(aoa_max) * v ** 2
+    lift = 0.5 * rho * S * f_cl(aoa_max) * v ** 2
 
-    dv = min_accel * dt
+    SFx = T_aero - drag - weight * np.sin(gamma_rad)
 
-    if aoa < aoa_max:
-        teta = q * dt + teta0
-    else:
+    accel = SFx / mass
 
-        teta = teta0
+    dgamma = (thrust * np.sin(np.radians(aoa_max)) + lift - weight) / (mass * v) * dt
 
-    gamma = teta - aoa
+    gamma_rad += dgamma
+    gamma = np.degrees(gamma_rad)
+    teta = aoa_max + gamma
 
-    v_z = v * np.sin(np.radians(gamma))
+    v_z = v * np.sin(gamma_rad)
 
-    a_z = min_accel * np.sin(np.radians(gamma))
+    dv = accel * dt
 
-    dh = a_z*dt**2 + v_z * dt
+    dh = v * np.sin(gamma_rad) * dt  # a_z*dt**2 + v_z * dt
 
-    dx = dh * np.tan(np.radians(gamma))
-
-    height += uc.m2ft(dh)
+    dx = v * np.cos(gamma_rad) * dt  # dh * np.tan(gamma_rad)
 
     # update
     x += dx
     v += dv
     t += dt
-
-
+    height += uc.m2ft(dh)
     teta0 = teta
-    aoa0 = min(teta, aoa_max)
     v_kt = uc.ms2kt(v)
+
     if gamma > 2:
         print('error: gamma overshoots')
         break
@@ -232,6 +202,7 @@ while height < 35.:
     if v_kt >= v2min:
         print(f'V2min reached at {round(height, 2)}ft!')
         break
+
 print(f'Height {height} ft')
 print(f'Gamma {gamma} deg')
 print(f'Teta {teta} deg')
@@ -240,8 +211,10 @@ print(f'Distance {x} m\n')
 while height < 35.:
 
     teta = teta_target
+    aoa = teta - gamma
+    drag = 0.5 * rho * S * f_cd(aoa) * v ** 2
     gamma = (thrust - drag) / weight
-    aoa = teta - uc.rad2deg(gamma)
+
     lift = weight * np.cos(gamma)
     v_z = v * np.sin(gamma)
     dh = v_z * dt
@@ -251,7 +224,6 @@ while height < 35.:
     x += dx
     t += dt
     height += uc.m2ft(dh)
-    # print(f'Distance {x} m\n')
 
 print(f'Height {round(height, 2)} ft')
 print(f'Distance {round(x, 2)} m')
@@ -269,6 +241,7 @@ y_data = [config_data['alpha_min'], config_data['alpha_max']]
 f_aoa = interp1d(x_data, y_data)
 aoa_35ft = f_aoa(cd_35ft)
 
+# TODO create a loop in order fro ac speed to converge to the requirement(easy)
 # Extra requirement from JAR 25.121(b)
 gamma_jar_req = uc.perc2rad(2.4)
 v_min_grad = (2 * (thrust * np.cos(np.radians(aoa_35ft)) - weight*np.sin(gamma_jar_req)) / (rho * S * cd_35ft)) ** .5
